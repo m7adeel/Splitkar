3830f16c1126f01a6d7edcc7f21cdad03bd3f42a
Muhammad Adeel <m7adeel@gmail.com>
2024-09-28 23:15:35 +0500

    add: bottom navigation bar

--- src/router/Home.js
+++ src/router/Home.js
@@ -1,21 +1,233 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { TouchableOpacity, Animated, StyleSheet } from 'react-native';
+import React, { useRef, useEffect } from 'react';
+
+import { CurvedBottomBarExpo } from 'react-native-curved-bottom-bar';
 
 import LandingPage from '../screens/LandingPage';
 import AddBill from '../screens/AddBill';
 import Profile from '../screens/Profile';
 
+// Icons
+import {
+  MaterialCommunityIcons,
+  MaterialIcons,
+  Ionicons
+} from '@expo/vector-icons';
+
 const Tab = createBottomTabNavigator();
 
-function Home() {
+const _renderIcon = (routeName, selectedTab) => {
+  let icon = '';
+
+  switch (routeName) {
+    case 'LandingPage':
+      icon = 'home';
+      break;
+    case 'Profile':
+      icon = 'groups';
+      break;
+    case 'Notification':
+      icon = 'notifications';
+      break;
+    case 'Wallet':
+      icon = 'wallet';
+      break;
+  }
+
+  if(routeName === 'Profile') {
+    return (
+      <MaterialIcons
+        name={icon}
+        size={28}
+        color={routeName === selectedTab ? '#ffdc64' : '#3c3f44'}
+      />
+    )
+  }
+
+  if(routeName === selectedTab) {
+    icon = icon + '-sharp';
+  }
+
   return (
-    <Tab.Navigator screenOptions={{
-      headerShown: false
-    }}>
-      <Tab.Screen name="LandingPage" component={LandingPage} />
-      <Tab.Screen name="AddBill" component={AddBill} />
+    <Ionicons
+      name={icon}
+      size={25}
+      color={routeName === selectedTab ? '#ffdc64' : '#3c3f44'}
+    />
+  );
+};
+
+const renderTabBar = ({ routeName, selectedTab, navigate }) => {
+  return (
+    <TouchableOpacity
+      onPress={() => navigate(routeName)}
+      style={styles.tabbarItem}
+    >
+      {_renderIcon(routeName, selectedTab)}
+    </TouchableOpacity>
+  );
+};
+
+const AddButton = ({ children, onPress, accessibilityState }) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      className="bg-[#ffdc64] rounded-full flex items-center justify-center border-4 border-[#392c16]"
+      style={{
+        top: -30,
+        width: 70,
+        height: 70,
+      }}
+    >
+      <Animatable.View
+        ref={viewRef}
+        duration={2000}
+      >
+        {children}
+      </Animatable.View>
+    </TouchableOpacity>
+  );
+}
+
+function Home() {
+  const screenOptions = {
+    tabBarShowLabel: false,
+    headerShown: false,
+    tabBarStyle: {
+      backgroundColor: '#1e151a',
+      borderTopColor: '#1e151a'
+    },
+  }
+
+  return (
+    <CurvedBottomBarExpo.Navigator
+      type="UP"
+      screenOptions={screenOptions}
+      style={styles.bottomBar}
+      shadowStyle={styles.shawdow}
+      height={55}
+      circleWidth={50}
+      borderWidth={1}
+      bgColor="#191d28"
+      borderColor="#27262c"
+      borderTopLeftRight
+      initialRouteName="LandingPage"
+      hide
+      renderCircle={({ selectedTab, navigate }) => (
+        <Animated.View
+          className="flex items-center justify-center p-1 rounded-full"
+          style={styles.btnCircleUp}>
+          <TouchableOpacity
+            className="flex items-center justify-center bg-[#ffdc64] rounded-full h-full w-full"
+            
+          >
+            <Ionicons name={'add'} color="#1b1104" size={25} />
+          </TouchableOpacity>
+        </Animated.View>
+      )}
+      tabBar={renderTabBar}
+    >
+      <CurvedBottomBarExpo.Screen
+        name="LandingPage"
+        position='LEFT'
+        component={() => <LandingPage />}
+      />
+      <CurvedBottomBarExpo.Screen
+        name="Wallet"
+        position='LEFT'
+        component={() => <LandingPage />}
+      />
+      <CurvedBottomBarExpo.Screen
+        position='RIGHT'
+        name="Profile" component={() => <Profile />}
+      />
+      <CurvedBottomBarExpo.Screen
+        position='RIGHT'
+        name="Notification" component={() => <Profile />}
+      />
+    </CurvedBottomBarExpo.Navigator>
+  )
+
+  return (
+    <Tab.Navigator screenOptions={screenOptions}>
+      <Tab.Screen name="LandingPage" component={LandingPage}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen name="AddBill" component={AddBill}
+        options={{
+          tabBarLabel: 'Add Bill',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="plus" color={color} size={size} />
+          ),
+          tabBarButton: (props) => (
+            <AddButton {...props} />
+          )
+        }}
+      />
       <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
   );
 }
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  shawdow: {
+    shadowColor: '#DDDDDD',
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 5,
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  bottomBar: {},
+  btnCircleUp: {
+    width: 60,
+    height: 60,
+    bottom: 24,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 1,
+  },
+  imgCircle: {
+    width: 30,
+    height: 30,
+    tintColor: 'gray',
+  },
+  tabbarItem: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  img: {
+    width: 30,
+    height: 30,
+  },
+  screen1: {
+    flex: 1,
+    backgroundColor: '#BFEFFF',
+  },
+  screen2: {
+    flex: 1,
+    backgroundColor: '#FFEBCD',
+  },
+});
 
 export default Home;
